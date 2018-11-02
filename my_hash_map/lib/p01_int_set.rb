243c9ffda16b8c5241062b6324fcc64a530dc94d
kopecmark <mark.kopec@gmail.com>
2018-11-02 13:50:50 -0700

    Finish part 1

--- my_hash_map/lib/p01_int_set.rb
+++ my_hash_map/lib/p01_int_set.rb
@@ -1,14 +1,22 @@
+require 'byebug'
+
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max) {false}
   end
 
   def insert(num)
+    raise "Out of bounds" if num > @max - 1 || num < 0
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
@@ -24,15 +32,22 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    # debugger
+    @store[num % @num_buckets] << num
+    true
   end
 
   def remove(num)
+    @store[num % @num_buckets].delete(num)
   end
 
   def include?(num)
+    return true if @store[num % @num_buckets].include?(num)
+    false
   end
 
   private
@@ -55,12 +70,24 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count == num_buckets
+
+    unless include?(num)
+      @store[num % num_buckets] << num
+      @count += 1
+    end
+    # @count
   end
 
   def remove(num)
+    if include?(num)
+      @store[num % num_buckets].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    @store[num % num_buckets].include?(num)
   end
 
   private
@@ -74,5 +101,13 @@ class ResizingIntSet
   end
 
   def resize!
+    temp = @store
+    @store = Array.new(count * 2) { Array.new }
+    temp.each do |arr|
+      arr.each do |el|
+        @store[el % num_buckets] << el
+      end
+    end
+
   end
 end
