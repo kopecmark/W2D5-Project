7207367324848d82c445d0ac19b0ae16e5847da6
kopecmark <mark.kopec@gmail.com>
2018-11-02 16:15:41 -0700

    Complete linked list

--- my_hash_map/lib/p04_linked_list.rb
+++ my_hash_map/lib/p04_linked_list.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -17,10 +18,21 @@ class Node
     # optional but useful, connects previous link to next link
     # and removes self from list.
   end
+
+  def inspect
+    "Key: #{self.key} Val: #{self.val} Prev: #{self.prev} Next: #{self.next}"
+  end
 end
 
 class LinkedList
+
+  include Enumerable
+
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +41,68 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    return true if @head.next == @tail
+    false
   end
 
   def get(key)
+    self.each do |node|
+      return node.val if node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    # debugger
+    self.each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+    # debugger
+    temp = Node.new(key, val)
+    temp.next = @tail
+    temp.prev = @tail.prev
+    @tail.prev.next = temp
+    @tail.prev = temp
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end 
   end
 
   def remove(key)
+    temp = nil
+    self.each do |node|
+      temp = node if node.key == key
+    end
+    temp.prev.next = temp.next
+    temp.next.prev = temp.prev
+
   end
 
-  def each
+  def each(&prc)
+    # debugger
+    result = []
+    next_node = @head.next
+    until next_node == @tail
+      # result << next_node.val
+      result << prc.call(next_node)
+      next_node = next_node.next
+    end
+    result
   end
 
   # uncomment when you have `each` working and `Enumerable` included
