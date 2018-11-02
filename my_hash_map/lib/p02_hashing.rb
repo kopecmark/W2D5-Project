2228dd1c7041514ab3d01067f2826015e92ee868
kopecmark <mark.kopec@gmail.com>
2018-11-02 14:05:27 -0700

    Complete p02

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    total = 0
+    self.each.with_index {|el, idx| total+= el.hash*(idx+1)}
+    total
   end
 end
 
 class String
   def hash
+    total = 0
+    self.chars.each.with_index {|el, idx| total+= el.ord.hash*(idx+1)}
+    total
   end
 end
 
@@ -16,6 +22,9 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    total = 0
+    self.each {|k, v| total+= (k.to_s.hash + v.hash)}
+    total
+
   end
 end
